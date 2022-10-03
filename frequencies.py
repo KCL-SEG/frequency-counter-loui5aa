e8fd2f9913e1d31cd2e55425eb969a44648e890e
Louisa Scott <louisaj3996@gmail.com>
2022-10-03 10:33:21 +0100

    Second attempt at Frequencies Exercise

--- frequencies.py
+++ frequencies.py
@@ -7,15 +7,19 @@ from operator import truediv
 def frequencies(items):
     frequencies = {}
     for x in items:
+        x= str(x)
         if len(frequencies) == 0: #if dictionary empty
-            frequencies[str(x)]=1
+            frequencies[x]=1
         else: #if dictionary has contents
             founditem = 0
             for y in frequencies:
                 if x == y: #if the item is in the dictionary
-                    frequencies[y]=+ 1
+                    print(x + " found in dictionary")
+                    frequencies[y]= frequencies[y] + 1
                     founditem = 1
+            
             if founditem == 0:
-                frequencies[str(x)] = 1
+                print(x + " not in dictionary")
+                frequencies[x] =  1
     print("Loading done")
     return frequencies
